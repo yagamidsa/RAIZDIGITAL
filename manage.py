e507337a243162fac16764cddfc25ee51b7eb8d4
David Alipio <david.alipio@kantar.com>
2025-05-22 22:34:06 -0500

    bug

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative de tatasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'raizdigital.settings.production')
     try:
         from django.core.management import execute_from_command_line
