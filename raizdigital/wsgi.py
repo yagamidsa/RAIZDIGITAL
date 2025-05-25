bf9db714a5f16df0add930b4e43d51222645eb81
David Alipio <david.alipio@kantar.com>
2025-05-25 11:01:32 -0500

    registrov23

--- raizdigital/wsgi.py
+++ raizdigital/wsgi.py
@@ -10,5 +10,5 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'raizdigital.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'raizdigital.settings.local')
 application = get_wsgi_application()
